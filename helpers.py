b0a359fcb48eafbeba40394c2b04c37dd39b006c
cami3 <43848863+cami3@users.noreply.github.com>
2023-03-10 17:26:15 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -11,9 +11,6 @@ from qiime2.plugins.dada2.methods import denoise_single
 import os
 import tempfile
 
-#os.environ['TMPDIR'] = '/Volumes/HD_ext_CAMI/streamlit_apps/tmp_dir/'
-#TMPDIR = os.getenv('TMPDIR')
-
 @st.experimental_singleton(show_spinner=True)
 def import_paired_end_fastq_gz_files(_filepath):
     '''
@@ -87,24 +84,44 @@ def quality_filter_paired_end(demux_joined, min_quality, quality_window):
     df_q_filter = df_q_filter.rename(cols_renameing, axis=1)
     return demux_filter, df_q_filter, secure_temp_dir_q_filter_summary
 
+@st.cache_resource
+def app_alpha_rare_curves(_table, max_depth, metrics):
+    alpha_rare_curves = alpha_rarefaction(table = _table, max_depth=max_depth, metrics=metrics)
+    return alpha_rare_curves
+
+
+@st.cache_resource
+def app_align_to_tree_mafft_fasttree(_sequences, _table, sampling_depth, _metadata):
+    result_alignment = align_to_tree_mafft_fasttree(sequences = _sequences)
+    seqs_alignment = result_alignment.alignment
+    masked_alignment = result_alignment.masked_alignment
+    tree = result_alignment.tree
+    rooted_tree = result_alignment.rooted_tree
+
+    secure_temp_dir_phylogenetic_tree = tempfile.mkdtemp(prefix="temp_", suffix="_phylogenetic_tree")
+    rooted_tree.export_data(secure_temp_dir_phylogenetic_tree)
+    
+    result_core_metrics = core_metrics_phylogenetic(table=_table, phylogeny=rooted_tree, sampling_depth=sampling_depth, metadata=_metadata)
+    core_metr_phylo = result_core_metrics
+    return seqs_alignment, masked_alignment, tree, rooted_tree, core_metr_phylo, secure_temp_dir_phylogenetic_tree
 
 
 @st.cache(show_spinner=True)
-def dada2_denoise_single_joined(demux_filter, N, trim_TF):
+def dada2_denoise_single_joined(_demux_filter, N, trim_TF):
     trunc_len = N
     pooling_method = "independent"
     chimera_method = "consensus"
     n_threads = 0 # all available cores
     if trim_TF:
         dada2_sequences = dada2.methods.denoise_single(
-            demultiplexed_seqs = demux_filter,
+            demultiplexed_seqs = _demux_filter,
             trunc_len=trunc_len,
             pooling_method=pooling_method, 
             chimera_method=chimera_method,
             n_threads=n_threads)
     else:
         dada2_sequences = dada2.methods.denoise_single(
-            demultiplexed_seqs = demux_filter,
+            demultiplexed_seqs = _demux_filter,
             trunc_len=0, # disable trimming
             pooling_method=pooling_method,
             chimera_method=chimera_method,
@@ -116,7 +133,7 @@ def dada2_denoise_single_joined(demux_filter, N, trim_TF):
 
 
 @st.cache(show_spinner=True)
-def deblur_denoise_trim_paired_end(demux_filter, N, trim_TF):
+def deblur_denoise_trim_paired_end(_demux_filter, N, trim_TF):
     '''
     Performs denoising of data and trimming at position N, which is defined by the user based on
     the filter stats --> position in the reads where the median quality score drops too low.
@@ -127,25 +144,18 @@ def deblur_denoise_trim_paired_end(demux_filter, N, trim_TF):
     '''
     if trim_TF:
         deblur_sequences = deblur.methods.denoise_16S(
-            demux_filter,
+            _demux_filter,
             trim_length=N, # disable trimming
             sample_stats=True, 
             jobs_to_start=57)
     else:
         deblur_sequences = deblur.methods.denoise_16S(
-            demux_filter,
+            _demux_filter,
             trim_length=-1, # disable trimming
             sample_stats=True,
             jobs_to_start=57)
     return deblur_sequences
 
-@st.experimental_memo(show_spinner=True)
-def app_alpha_rare_curves(_table, max_depth, metrics):
-    alpha_rare_curves = qiime2.plugins.diversity.visualizers.alpha_rarefaction(
-        table=_table, 
-        max_depth=max_depth,
-        metrics=metrics)
-    return alpha_rare_curves
 
 @st.experimental_memo(show_spinner=True)
 def import_SequencesWithQuality(_filepath):
