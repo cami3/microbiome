10caea13d71c02749ccc0b036d53886c054155ae
cami3 <43848863+cami3@users.noreply.github.com>
2023-06-15 21:42:24 +0200

    Update removed caching

--- italiano.py
+++ italiano.py
@@ -314,160 +314,169 @@ hypervar_regions_plchldr = sidemenus.empty()
 ######################## FUNZIONI UTILI ########################
 # Funzioni copiate da helper.py file
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def import_paired_end_fastq_gz_files(_filepath):
-    '''
-    Import R1 and R2 fastq.gz files for all samples in the project.
-    fastq files must be in the Casava 1.8 format.
-    '''
-    paired_end_sequences = qiime2.Artifact.import_data(
-        'SampleData[PairedEndSequencesWithQuality]', 
-        _filepath,
-        'CasavaOneEightSingleLanePerSampleDirFmt')
-    return paired_end_sequences
+	'''
+	Import R1 and R2 fastq.gz files for all samples in the project.
+	fastq files must be in the Casava 1.8 format.
+	'''
+	with st.spinner('Analisi in corso'):
+		paired_end_sequences = qiime2.Artifact.import_data(
+			'SampleData[PairedEndSequencesWithQuality]', 
+			_filepath,
+			'CasavaOneEightSingleLanePerSampleDirFmt')
+	return paired_end_sequences
 
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def import_single_end_fastq_gz_files(_filepath):
-    '''
-    Import R1 fastq.gz files for all samples in the project.
-    fastq files must be in the Casava 1.8 format.
-    '''
-    single_end_sequences = qiime2.Artifact.import_data(
-        'SampleData[SequencesWithQuality]', 
-        _filepath,
-        'CasavaOneEightSingleLanePerSampleDirFmt')
-    return single_end_sequences
+	'''
+	Import R1 fastq.gz files for all samples in the project.
+	fastq files must be in the Casava 1.8 format.
+	'''
+	with st.spinner('Analisi in corso'):
+		single_end_sequences = qiime2.Artifact.import_data(
+			'SampleData[SequencesWithQuality]', 
+			_filepath,
+			'CasavaOneEightSingleLanePerSampleDirFmt')
+	return single_end_sequences
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def vsearch_join_pairs(_paired_end_sequences, minovlen, minmergelen, maxmergelen, maxee, maxdiffs):#minovlen=20, minmergelen=460, maxmergelen=480, maxee=1, maxdiffs=5):
-    '''
-    Performs joining of paired end reads based on given paramenters.
-    Default parameters are set for reads' length of 250 bases (v2 Illumina MiSeq kit)
-    '''
-    demux_joined = vsearch.methods.merge_pairs(
-        demultiplexed_seqs = _paired_end_sequences,
-        minovlen=minovlen,
-        minmergelen=minmergelen,
-        maxmergelen=maxmergelen,
-        maxee=maxee,
-        maxdiffs=maxdiffs)
-    return demux_joined
+	'''
+	Performs joining of paired end reads based on given paramenters.
+	Default parameters are set for reads' length of 250 bases (v2 Illumina MiSeq kit)
+	'''
+	with st.spinner('Analisi in corso'):
+		demux_joined = vsearch.methods.merge_pairs(
+			demultiplexed_seqs = _paired_end_sequences,
+			minovlen=minovlen,
+			minmergelen=minmergelen,
+			maxmergelen=maxmergelen,
+			maxee=maxee,
+			maxdiffs=maxdiffs)
+	return demux_joined
 
 
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def quality_filter_paired_end(_demux_joined, min_quality, quality_window):
-    '''
-    Performs quality filtering of paired-end fastq reads based on phred64 Illumina quality score.
-    '''
-    demux_filter = quality_filter.methods.q_score(demux=_demux_joined, min_quality=min_quality, quality_window=quality_window)
-    secure_temp_dir_q_filter_summary = tempfile.mkdtemp(prefix="temp_", suffix="_q_filter_summary")
-    filter_stats = metadata.visualizers.tabulate(demux_filter.filter_stats.view(qiime2.Metadata))
-    filter_stats.visualization.export_data(secure_temp_dir_q_filter_summary)
+	'''
+	Performs quality filtering of paired-end fastq reads based on phred64 Illumina quality score.
+	'''
+	with st.spinner('Analisi in corso'):
+		demux_filter = quality_filter.methods.q_score(demux=_demux_joined, min_quality=min_quality, quality_window=quality_window)
+		secure_temp_dir_q_filter_summary = tempfile.mkdtemp(prefix="temp_", suffix="_q_filter_summary")
+		filter_stats = metadata.visualizers.tabulate(demux_filter.filter_stats.view(qiime2.Metadata))
+		filter_stats.visualization.export_data(secure_temp_dir_q_filter_summary)
 
-    df_q_filter = pd.read_csv(secure_temp_dir_q_filter_summary+'/metadata.tsv', sep='\t' , skiprows=[1], index_col='sample-id')
-    df_q_filter.columns = ["total_input_reads", 
-        "total_retained_reads", 
-        "reads_truncated", 
-        "reads_too_short_after_truncation",
-        "reads_exceeding_maximum_ambiguous_bases"]
-    df_q_filter = df_q_filter.eval("""
-        perc_retained_reads = ((total_retained_reads * 100)/total_input_reads)
-        """)
-    cols_renameing = {
-        "total_input_reads": 'totale_sequenze_iniziali', 
-        "total_retained_reads": 'totale_sequenze_accettabili', 
-        "reads_truncated": 'sequenze_troncate', 
-        "reads_too_short_after_truncation": 'sequenze_troncate_troppo_corte_scartate',
-        "reads_exceeding_maximum_ambiguous_bases": ('sequenze_con_oltre_%s_basi_ambigue_scartate' %(quality_window)),
-        'perc_retained_reads': 'percentuale_sequenze_accettabili'
-        }
-    
-    df_q_filter = df_q_filter.rename(cols_renameing, axis=1)
-    return demux_filter, df_q_filter, secure_temp_dir_q_filter_summary
+		df_q_filter = pd.read_csv(secure_temp_dir_q_filter_summary+'/metadata.tsv', sep='\t' , skiprows=[1], index_col='sample-id')
+		df_q_filter.columns = ["total_input_reads", 
+			"total_retained_reads", 
+			"reads_truncated", 
+			"reads_too_short_after_truncation",
+			"reads_exceeding_maximum_ambiguous_bases"]
+		df_q_filter = df_q_filter.eval("""
+			perc_retained_reads = ((total_retained_reads * 100)/total_input_reads)
+			""")
+		cols_renameing = {
+			"total_input_reads": 'totale_sequenze_iniziali', 
+			"total_retained_reads": 'totale_sequenze_accettabili', 
+			"reads_truncated": 'sequenze_troncate', 
+			"reads_too_short_after_truncation": 'sequenze_troncate_troppo_corte_scartate',
+			"reads_exceeding_maximum_ambiguous_bases": ('sequenze_con_oltre_%s_basi_ambigue_scartate' %(quality_window)),
+			'perc_retained_reads': 'percentuale_sequenze_accettabili'
+			}
+		
+		df_q_filter = df_q_filter.rename(cols_renameing, axis=1)
+	return demux_filter, df_q_filter, secure_temp_dir_q_filter_summary
 
-@st.cache_resource
+#@st.cache_resource
 def app_alpha_rare_curves(_table, max_depth, metrics):
-	alpha_rare_curves = alpha_rarefaction(table = _table, max_depth=max_depth, metrics=metrics)
+	with st.spinner('Analisi in corso'):
+		alpha_rare_curves = alpha_rarefaction(table = _table, max_depth=max_depth, metrics=metrics)
 	return alpha_rare_curves
 
 
-@st.cache_resource
+#@st.cache_resource
 def app_align_to_tree_mafft_fasttree(_sequences, _table, sampling_depth, _metadata):
-	result_alignment = align_to_tree_mafft_fasttree(sequences = _sequences)
-	seqs_alignment = result_alignment.alignment
-	masked_alignment = result_alignment.masked_alignment
-	tree = result_alignment.tree
-	rooted_tree = result_alignment.rooted_tree
+	with st.spinner('Analisi in corso'):
+		result_alignment = align_to_tree_mafft_fasttree(sequences = _sequences)
+		seqs_alignment = result_alignment.alignment
+		masked_alignment = result_alignment.masked_alignment
+		tree = result_alignment.tree
+		rooted_tree = result_alignment.rooted_tree
 
-	result_core_metrics = core_metrics_phylogenetic(table=_table, phylogeny=rooted_tree, sampling_depth=sampling_depth, metadata=_metadata)
-	core_metr_phylo = result_core_metrics
+		result_core_metrics = core_metrics_phylogenetic(table=_table, phylogeny=rooted_tree, sampling_depth=sampling_depth, metadata=_metadata)
+		core_metr_phylo = result_core_metrics
 	return seqs_alignment, masked_alignment, tree, rooted_tree, core_metr_phylo
 
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def dada2_denoise_single_joined(_demux_filter, N, trim_TF):
-    trunc_len = N
-    pooling_method = "independent"
-    chimera_method = "consensus"
-    n_threads = 0 # all available cores
-    if trim_TF:
-        dada2_sequences = dada2.methods.denoise_single(
-            demultiplexed_seqs = _demux_filter,
-            trunc_len=trunc_len,
-            pooling_method=pooling_method, 
-            chimera_method=chimera_method,
-            n_threads=n_threads)
-    else:
-        dada2_sequences = dada2.methods.denoise_single(
-            demultiplexed_seqs = _demux_filter,
-            trunc_len=0, # disable trimming
-            pooling_method=pooling_method,
-            chimera_method=chimera_method,
-            n_threads=n_threads)
-    return dada2_sequences
+	with st.spinner('Analisi in corso'):
+		trunc_len = N
+		pooling_method = "independent"
+		chimera_method = "consensus"
+		n_threads = 0 # all available cores
+		if trim_TF:
+			dada2_sequences = dada2.methods.denoise_single(
+				demultiplexed_seqs = _demux_filter,
+				trunc_len=trunc_len,
+				pooling_method=pooling_method, 
+				chimera_method=chimera_method,
+				n_threads=n_threads)
+		else:
+			dada2_sequences = dada2.methods.denoise_single(
+				demultiplexed_seqs = _demux_filter,
+				trunc_len=0, # disable trimming
+				pooling_method=pooling_method,
+				chimera_method=chimera_method,
+				n_threads=n_threads)
+	return dada2_sequences
 
 
 
 
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def deblur_denoise_trim_paired_end(_demux_filter, N, trim_TF):
-    '''
-    Performs denoising of data and trimming at position N, which is defined by the user based on
-    the filter stats --> position in the reads where the median quality score drops too low.
-    Only forward reads are supported at this time, so perform paired-end reads joining first.
-    If trim_TF is True, trimming is enabled at length N.
-    Otherwise, if trim_TF is False, disable trimming.
-    "Deblur operates only on same length sequences. " from the web https://forum.qiime2.org/t/deblur-plugin-error/2172
-    '''
-    if trim_TF:
-        deblur_sequences = deblur.methods.denoise_16S(
-            _demux_filter,
-            trim_length=N, # disable trimming
-            sample_stats=True, 
-            jobs_to_start=57)
-    else:
-        deblur_sequences = deblur.methods.denoise_16S(
-            _demux_filter,
-            trim_length=-1, # disable trimming
-            sample_stats=True,
-            jobs_to_start=57)
-    return deblur_sequences
+	'''
+	Performs denoising of data and trimming at position N, which is defined by the user based on
+	the filter stats --> position in the reads where the median quality score drops too low.
+	Only forward reads are supported at this time, so perform paired-end reads joining first.
+	If trim_TF is True, trimming is enabled at length N.
+	Otherwise, if trim_TF is False, disable trimming.
+	"Deblur operates only on same length sequences. " from the web https://forum.qiime2.org/t/deblur-plugin-error/2172
+	'''
+	with st.spinner('Analisi in corso'):
+		if trim_TF:
+			deblur_sequences = deblur.methods.denoise_16S(
+				_demux_filter,
+				trim_length=N, # disable trimming
+				sample_stats=True, 
+				jobs_to_start=57)
+		else:
+			deblur_sequences = deblur.methods.denoise_16S(
+				_demux_filter,
+				trim_length=-1, # disable trimming
+				sample_stats=True,
+				jobs_to_start=57)
+	return deblur_sequences
 
 
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def import_SequencesWithQuality(_filepath):
-    '''
-    Import R1 fastq.gz files for all samples in the project.
-    fastq files must be in the Casava 1.8 format.
-    '''
-    single_end_sequences = qiime2.Artifact.import_data(
-        'SampleData[SequencesWithQuality]', 
-        _filepath,
-        'SingleLanePerSampleSingleEndFastqDirFmt')
-    return single_end_sequences
+	'''
+	Import R1 fastq.gz files for all samples in the project.
+	fastq files must be in the Casava 1.8 format.
+	'''
+	with st.spinner('Analisi in corso'):
+		single_end_sequences = qiime2.Artifact.import_data(
+			'SampleData[SequencesWithQuality]', 
+			_filepath,
+			'SingleLanePerSampleSingleEndFastqDirFmt')
+	return single_end_sequences
 
 
 @st.cache_resource
@@ -615,19 +624,20 @@ def myround(x, base=10):
 
 
 # funzione cached per calcolare le statistiche riassuntive dei dati grezzi caricati dall'utente
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def app_demux_visualizers_summarize(_sequences):
 	'''
 	Funzione cached per calcolare le statistiche riassuntive 
 	dei dati grezzi caricati dall'utente
 	'''
-	demux_summary = demux.visualizers.summarize(_sequences)
-	secure_temp_dir_demux_summary = tempfile.mkdtemp(prefix="temp_", suffix="_demux_summary")
-	demux_summary.visualization.export_data(secure_temp_dir_demux_summary)
-	demux_summary = pd.read_csv(secure_temp_dir_demux_summary+'/per-sample-fastq-counts.tsv', sep='\t' , index_col='sample ID')
-	
-	with open(secure_temp_dir_demux_summary+"/quality-plot.html", 'r', encoding='utf-8') as HtmlFile:
-		source_code_demux_summary = HtmlFile.read() 
+	with st.spinner('Analisi in corso'):
+		demux_summary = demux.visualizers.summarize(_sequences)
+		secure_temp_dir_demux_summary = tempfile.mkdtemp(prefix="temp_", suffix="_demux_summary")
+		demux_summary.visualization.export_data(secure_temp_dir_demux_summary)
+		demux_summary = pd.read_csv(secure_temp_dir_demux_summary+'/per-sample-fastq-counts.tsv', sep='\t' , index_col='sample ID')
+		
+		with open(secure_temp_dir_demux_summary+"/quality-plot.html", 'r', encoding='utf-8') as HtmlFile:
+			source_code_demux_summary = HtmlFile.read() 
 
 	return demux_summary, source_code_demux_summary, secure_temp_dir_demux_summary
 
@@ -669,26 +679,27 @@ def clear_files_and_cache_button_callback():
 		
 
 # Funzione per effettuare la fusione delle reads paired-end con vsearch e visualizzare i risultati
-@st.cache_resource(show_spinner=True)
+#@st.cache_resource(show_spinner=True)
 def form_callback_vsearch_join(_paired_end_sequences, imported_sequences_temp_dir, secure_temp_dir_demux_summary):
-	minovlen = st.session_state.minovlen_slider
-	minmergelen = st.session_state.minmergelen_slider
-	maxmergelen = st.session_state.maxmergelen_slider
-	maxee = st.session_state.maxee_slider
-	maxdiffs = st.session_state.maxdiffs_slider
-	
-	# todo
+	with st.spinner('Analisi in corso'):
+		minovlen = st.session_state.minovlen_slider
+		minmergelen = st.session_state.minmergelen_slider
+		maxmergelen = st.session_state.maxmergelen_slider
+		maxee = st.session_state.maxee_slider
+		maxdiffs = st.session_state.maxdiffs_slider
+		
+		# todo
 
-	demux_joined = vsearch_join_pairs(_paired_end_sequences, minovlen, minmergelen, maxmergelen, maxee, maxdiffs)
-	err = 'prova'
+		demux_joined = vsearch_join_pairs(_paired_end_sequences, minovlen, minmergelen, maxmergelen, maxee, maxdiffs)
+		err = 'prova'
 
-	secure_temp_dir_joined_summary = tempfile.mkdtemp(prefix="temp_", suffix="_demux_joined_summary")
-	demux_joined_summary = demux.visualizers.summarize(demux_joined.merged_sequences)
-	demux_joined_summary.visualization.export_data(secure_temp_dir_joined_summary)
-	df_demux_joined_summary = pd.read_csv(secure_temp_dir_joined_summary+'/per-sample-fastq-counts.tsv', sep='\t', index_col='sample ID')
-	joined_sequences = demux_joined.merged_sequences
-	pdf_joined_sequences = displayPDF(secure_temp_dir_joined_summary+'/demultiplex-summary-forward.pdf')
-	
+		secure_temp_dir_joined_summary = tempfile.mkdtemp(prefix="temp_", suffix="_demux_joined_summary")
+		demux_joined_summary = demux.visualizers.summarize(demux_joined.merged_sequences)
+		demux_joined_summary.visualization.export_data(secure_temp_dir_joined_summary)
+		df_demux_joined_summary = pd.read_csv(secure_temp_dir_joined_summary+'/per-sample-fastq-counts.tsv', sep='\t', index_col='sample ID')
+		joined_sequences = demux_joined.merged_sequences
+		pdf_joined_sequences = displayPDF(secure_temp_dir_joined_summary+'/demultiplex-summary-forward.pdf')
+		
 	return df_demux_joined_summary, joined_sequences, pdf_joined_sequences, secure_temp_dir_joined_summary, err
 
 # Funzione per creare il dataframe finale dai files OTU table e taxonomy table .tsv
@@ -1001,16 +1012,16 @@ def OTUs_annots_freqs(_idx_sample_grouping):
 	return taxa_counts_d, df1_d, tabella_df_l_l, warning_tab_num_of_OTUs_per_taxon
 
 	
-@st.cache_resource
+#@st.cache_resource
 def app_classify_hybrid_vsearch_sklearn(_query, _reference_reads, _reference_taxonomy, _classifier, reads_per_batch, randseed):
-	
-	seqs_classification = classify_hybrid_vsearch_sklearn(query=_query,
-		reference_reads=_reference_reads,
-		reference_taxonomy=_reference_taxonomy,
-		classifier=_classifier,
-		prefilter=False,
-		reads_per_batch=reads_per_batch,
-		randseed=randseed)
+	with st.spinner('Analisi in corso'):
+		seqs_classification = classify_hybrid_vsearch_sklearn(query=_query,
+			reference_reads=_reference_reads,
+			reference_taxonomy=_reference_taxonomy,
+			classifier=_classifier,
+			prefilter=False,
+			reads_per_batch=reads_per_batch,
+			randseed=randseed)
 	return seqs_classification
 
 @st.cache
